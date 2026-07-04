5d60bd40edd97762454a52556694c00af80e628e
MthunziSibiya <st10448156@myemeris.edu.za>
2026-07-04 17:25:12 +0200

    test: inspect Layout.render

--- scratch/test_render.py
+++ scratch/test_render.py
@@ -1,5 +1,5 @@
 import inspect
 from rich.layout import Layout
 
-print("Source code of Layout.__rich_console__:")
-print(inspect.getsource(Layout.__rich_console__))
+print("Source code of Layout.render:")
+print(inspect.getsource(Layout.render))
